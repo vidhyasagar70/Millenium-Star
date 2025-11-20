99ec74656a12decdd64641f42c1de2082ee993f2
vidhyasagar70 <vidhyas369@gmail.com>
2025-11-20 22:51:40 +0530

    ui fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -258,7 +258,7 @@ const HomePage = () => {
                 </section>
             </AnimatedContainer>
 
-            {/* <Testimonial /> */}
+            {/* <Testimonial />*/}
         </div>
     );
 };
